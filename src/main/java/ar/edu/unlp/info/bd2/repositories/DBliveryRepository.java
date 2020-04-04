0c739b8e6bbe1a794529b527fb4bf9662375e34b
Leonel Mandarino <leonelmandarino@gmail.com>
2020-04-04 18:32:51 -0300

    Add findProductBySimilarName to repository, change access to session

--- src/main/java/ar/edu/unlp/info/bd2/repositories/DBliveryRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/DBliveryRepository.java
@@ -1,10 +1,16 @@
 package ar.edu.unlp.info.bd2.repositories;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.TypedQuery;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.query.Query;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import ar.edu.unlp.info.bd2.model.Product;
 import ar.edu.unlp.info.bd2.model.User;
 
 public class DBliveryRepository {
@@ -18,46 +24,54 @@ public class DBliveryRepository {
 
 	public <T> Object findById(Class<T> entityClass, Long id) {
 		Session session = sessionFactory.getCurrentSession();
-		return session.find(entityClass, id);
+		Object obj = session.find(entityClass, id);
+		return obj;
 	}
 
 	public Object saveObject(Object obj) {
-		Session session = sessionFactory.openSession();
-		session.beginTransaction();
-
+		Session session = sessionFactory.getCurrentSession();
 		session.saveOrUpdate(obj);
-
-		session.getTransaction().commit();
-		session.close();
 		return obj;
 	}
 
 	public Object deleteObject(Object obj) {
 		Session session = sessionFactory.getCurrentSession();
-		session.beginTransaction();
-
 		session.delete(obj);
-
-		session.getTransaction().commit();
-		session.close();
 		return obj;
 	}
 
 	public User findByEmail(String email) {
+		String hql = "SELECT u FROM User u WHERE u.email = :email";
+
 		Session session = sessionFactory.getCurrentSession();
-		String hql = "SELECT u FROM Users u WHERE u.email = :email";
-		Query<User> query = session.createQuery(hql, User.class);
+
+		TypedQuery<User> query = session.createQuery(hql, User.class);
 		query.setParameter("email", email);
-		User user = query.uniqueResult();
+		User user = ((Query<User>) query).uniqueResult();
+
 		return user;
 	}
 
 	public User findByUsername(String username) {
+		String hql = "SELECT u FROM User u WHERE u.username = :username";
+
 		Session session = sessionFactory.getCurrentSession();
-		String hql = "SELECT u FROM Users u WHERE u.username = :username";
-		Query<User> query = session.createQuery(hql, User.class);
+
+		TypedQuery<User> query = session.createQuery(hql, User.class);
 		query.setParameter("username", username);
-		User user = query.uniqueResult();
+		User user = ((Query<User>) query).uniqueResult();
+
 		return user;
+	}
+
+	public List<Product> findProductBySimilarName(String name) {
+		String hql = "FROM Product p WHERE p.name like :name";
+
+		Session session = sessionFactory.getCurrentSession();
+
+		Query query = session.createQuery(hql);
+		query.setParameter("name", "%" + name + "%");
+		List<Product> prods = query.list();
+		return prods;
 	}
 }
