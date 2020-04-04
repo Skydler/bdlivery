4491cc37c775cceac9b06c322b100b691d1be810
Leonel Mandarino <leonelmandarino@gmail.com>
2020-04-04 19:02:54 -0300

    Fix existent database or user error in script

--- createDatabase.sql
+++ createDatabase.sql
@@ -1,3 +1,5 @@
-CREATE USER 'grupo8'@'localhost' IDENTIFIED BY 'grupo8';
+DROP DATABASE IF EXISTS bd2_grupo8;
 CREATE DATABASE bd2_grupo8;
+CREATE USER IF NOT EXISTS 'grupo8'@'localhost' IDENTIFIED BY 'grupo8';
 GRANT ALL PRIVILEGES ON bd2_grupo8.* TO 'grupo8'@'localhost';
+FLUSH PRIVILEGES;
