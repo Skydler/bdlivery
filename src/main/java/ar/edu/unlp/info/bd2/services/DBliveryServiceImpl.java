fea61174e9e5838ef6c203ca7c12bd5963356686
Leonel Mandarino <leonelmandarino@gmail.com>
2020-04-13 01:32:50 -0300

    Add interface skeleton methods

--- src/main/java/ar/edu/unlp/info/bd2/services/DBliveryServiceImpl.java
+++ src/main/java/ar/edu/unlp/info/bd2/services/DBliveryServiceImpl.java
@@ -23,8 +23,80 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	public Order addProduct(Long order, Long quantity, Product product) throws DBliveryException {
+		Item item = new Item(quantity, product);
+		Order actualOrder = this.getOrderById(order)
+				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
+		List<Item> orderItems = actualOrder.getProducts();
+		orderItems.add(item);
+		repository.saveObject(actualOrder);
+		return actualOrder;
+	}
+
+	@Override
+	public boolean canCancel(Long order) throws DBliveryException {
+		Order actualOrder = this.getOrderById(order)
+				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
+		return actualOrder.isItemsEmpty() && actualOrder.getActualStatus().getStatus().equals(Order.PENDING);
+	}
+
+	@Override
+	public Order cancelOrder(Long order) throws DBliveryException {
+		Order actualOrder = this.getOrderById(order)
+				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
+		if (!this.canCancel(order)) {
+			throw new DBliveryException("The order can't be cancelled");
+		} else {
+			actualOrder.getStatus().add(new OrderStatus(Order.CANCELLED));
+		}
+		repository.saveObject(actualOrder);
+		return actualOrder;
+	}
+
+	@Override
+	public Order cancelOrder(Long order, Date date) throws DBliveryException {
+		Order actualOrder = this.getOrderById(order)
+				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
+		if (!this.canCancel(order)) {
+			throw new DBliveryException("The order can't be cancelled");
+		} else {
+			actualOrder.getStatus().add(new OrderStatus(Order.CANCELLED, date));
+		}
+		repository.saveObject(actualOrder);
+		return actualOrder;
+	}
+
+	@Override
+	public boolean canDeliver(Long order) throws DBliveryException {
+		Order actualOrder = this.getOrderById(order)
+				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
+		return !actualOrder.isItemsEmpty() && actualOrder.getActualStatus().getStatus().equals(Order.PENDING);
+	}
+
+	@Override
+	public boolean canFinish(Long id) throws DBliveryException {
+		Order actualOrder = this.getOrderById(id)
+				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
+		return !actualOrder.isItemsEmpty() && actualOrder.getActualStatus().getStatus().equals(Order.SENDING);
+	}
+
+	@Override
+	public Order createOrder(Date dateOfOrder, String address, Float coordX, Float coordY, User client) {
+		Order order = new Order(dateOfOrder, address, coordX, coordY, client);
+		repository.saveObject(order);
+		return order;
+	}
+
+	@Override
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier) {
 		Product prod = new Product(name, price, weight, supplier);
+		repository.saveObject(prod);
+		return prod;
+	}
+
+	@Override
+	public Product createProduct(String name, Float price, Float weight, Supplier supplier, Date date) {
+		Product prod = new Product(name, price, weight, supplier, date);
 		repository.saveObject(prod);
 		return prod;
 	}
@@ -44,63 +116,6 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
-	public Product updateProductPrice(Long id, Float price, Date startDate) throws DBliveryException {
-		Product prod = this.getProductById(id)
-				.orElseThrow(() -> new DBliveryException("No existe el producto para el id dado"));
-		prod.addPrice(price, startDate);
-		repository.saveObject(prod);
-		return prod;
-	}
-
-	@Override
-	public Optional<User> getUserById(Long id) {
-		User user = (User) repository.findById(User.class, id);
-		return Optional.of(user);
-	}
-
-	@Override
-	public Optional<User> getUserByEmail(String email) {
-		User user = repository.findByEmail(email);
-		return Optional.of(user);
-	}
-
-	@Override
-	public Optional<User> getUserByUsername(String username) {
-		User user = repository.findByUsername(username);
-		return Optional.of(user);
-	}
-
-	@Override
-	public Optional<Product> getProductById(Long id) {
-		Product prod = (Product) repository.findById(Product.class, id);
-		return Optional.of(prod);
-	}
-
-	@Override
-	public Optional<Order> getOrderById(Long id) {
-		Order order = (Order) repository.findById(Order.class, id);
-		return Optional.of(order);
-	}
-
-	@Override
-	public Order createOrder(Date dateOfOrder, String address, Float coordX, Float coordY, User client) {
-		Order order = new Order(dateOfOrder, address, coordX, coordY, client);
-		repository.saveObject(order);
-		return order;
-	}
-
-	@Override
-	public Order addProduct(Long order, Long quantity, Product product) throws DBliveryException {
-		Item item = new Item(quantity, product);
-		Order actualOrder = this.getOrderById(order)
-				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
-		List<Item> orderItems = actualOrder.getProducts();
-		orderItems.add(item);
-		repository.saveObject(actualOrder);
-		return actualOrder;
-	}
-
-	@Override
 	public Order deliverOrder(Long order, User deliveryUser) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
@@ -115,13 +130,14 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
-	public Order cancelOrder(Long order) throws DBliveryException {
+	public Order deliverOrder(Long order, User deliveryUser, Date date) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
-		if (!this.canCancel(order)) {
-			throw new DBliveryException("The order can't be cancelled");
+		if (!this.canDeliver(order)) {
+			throw new DBliveryException("The order can't be delivered");
 		} else {
-			actualOrder.getStatus().add(new OrderStatus(Order.CANCELLED));
+			actualOrder.setDeliveryUser(deliveryUser);
+			actualOrder.getStatus().add(new OrderStatus(Order.SENDING, date));
 		}
 		repository.saveObject(actualOrder);
 		return actualOrder;
@@ -141,24 +157,22 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
-	public boolean canCancel(Long order) throws DBliveryException {
+	public Order finishOrder(Long order, Date date) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
-		return actualOrder.isItemsEmpty() && actualOrder.getActualStatus().getStatus().equals(Order.PENDING);
+		if (!this.canFinish(order)) {
+			throw new DBliveryException("The order can't be finished");
+		} else {
+			actualOrder.getStatus().add(new OrderStatus(Order.DELIVERED, date));
+		}
+		repository.saveObject(actualOrder);
+		return actualOrder;
 	}
 
 	@Override
-	public boolean canFinish(Long id) throws DBliveryException {
-		Order actualOrder = this.getOrderById(id)
-				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
-		return !actualOrder.isItemsEmpty() && actualOrder.getActualStatus().getStatus().equals(Order.SENDING);
-	}
-
-	@Override
-	public boolean canDeliver(Long order) throws DBliveryException {
-		Order actualOrder = this.getOrderById(order)
-				.orElseThrow(() -> new DBliveryException("No existe la orden para el id dado"));
-		return !actualOrder.isItemsEmpty() && actualOrder.getActualStatus().getStatus().equals(Order.PENDING);
+	public List<User> get5LessDeliveryUsers() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 
 	@Override
@@ -168,9 +182,180 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	public List<Order> getAllOrdersMadeByUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product getBestSellingProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getCancelledOrdersInPeriod(Date startDate, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getDeliveredOrdersForUser(String username) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getDeliveredOrdersInPeriod(Date startDate, Date endDate) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getDeliveredOrdersSameDay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Order> getOrderById(Long id) {
+		Order order = (Order) repository.findById(Order.class, id);
+		return Optional.of(order);
+	}
+
+	@Override
+	public List<Order> getOrdersCompleteMorethanOneDay() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getOrderWithMoreQuantityOfProducts(Date day) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getPendingOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<Product> getProductById(Long id) {
+		Product prod = (Product) repository.findById(Product.class, id);
+		return Optional.of(prod);
+	}
+
+	@Override
 	public List<Product> getProductByName(String name) {
 		ArrayList<Product> prods = (ArrayList<Product>) repository.findProductBySimilarName(name);
 		return prods;
+	}
+
+	@Override
+	public List<Product> getProductIncreaseMoreThan100() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getProductsNotSold() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getProductsOnePrice() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Object[]> getProductsWithPriceAt(Date day) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getSentMoreOneHour() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Order> getSentOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getSoldProductsOn(Date day) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Supplier getSupplierLessExpensiveProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Supplier> getSuppliersDoNotSellOn(Date day) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Product> getTop10MoreExpensiveProducts() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<User> getTop6UsersMoreOrders() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<Supplier> getTopNSuppliersInSentOrders(int n) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Optional<User> getUserByEmail(String email) {
+		User user = repository.findByEmail(email);
+		return Optional.of(user);
+	}
+
+	@Override
+	public Optional<User> getUserById(Long id) {
+		User user = (User) repository.findById(User.class, id);
+		return Optional.of(user);
+	}
+
+	@Override
+	public Optional<User> getUserByUsername(String username) {
+		User user = repository.findByUsername(username);
+		return Optional.of(user);
+	}
+
+	@Override
+	public List<User> getUsersSpendingMoreThan(Float amount) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Product updateProductPrice(Long id, Float price, Date startDate) throws DBliveryException {
+		Product prod = this.getProductById(id)
+				.orElseThrow(() -> new DBliveryException("No existe el producto para el id dado"));
+		prod.addPrice(price, startDate);
+		repository.saveObject(prod);
+		return prod;
 	}
 
 }
