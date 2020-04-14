790a4e7a219c63ebd0f07134a92201ae59c05719
Leonel Mandarino <leonelmandarino@gmail.com>
2020-04-14 11:25:17 -0300

    Change sql dump for script

---
+++ createDatabase.sh
@@ -0,0 +1,6 @@
+mysql -u root -e "DROP DATABASE IF EXISTS bd2_grupo8;
+                  CREATE DATABASE bd2_grupo8;
+                  CREATE USER IF NOT EXISTS 'grupo8'@'localhost' IDENTIFIED BY 'grupo8';
+                  GRANT ALL PRIVILEGES ON bd2_grupo8.* TO 'grupo8'@'localhost';
+                  FLUSH PRIVILEGES;
+                  "
--- createDatabase.sql
+++
@@ -1,5 +0,0 @@
-DROP DATABASE IF EXISTS bd2_grupo8;
-CREATE DATABASE bd2_grupo8;
-CREATE USER IF NOT EXISTS 'grupo8'@'localhost' IDENTIFIED BY 'grupo8';
-GRANT ALL PRIVILEGES ON bd2_grupo8.* TO 'grupo8'@'localhost';
-FLUSH PRIVILEGES;
