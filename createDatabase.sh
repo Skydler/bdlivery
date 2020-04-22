719eba58c76767b66942699149e891222b5103c6
Leonel Mandarino <leonelmandarino@gmail.com>
2020-04-22 17:02:51 -0300

    Add password parameter to script

--- createDatabase.sh
+++ createDatabase.sh
@@ -1,4 +1,4 @@
-mysql -u root -e "DROP DATABASE IF EXISTS bd2_grupo8;
+mysql -u root -p -e "DROP DATABASE IF EXISTS bd2_grupo8;
                   CREATE DATABASE bd2_grupo8;
                   CREATE USER IF NOT EXISTS 'grupo8'@'localhost' IDENTIFIED BY 'grupo8';
                   GRANT ALL PRIVILEGES ON bd2_grupo8.* TO 'grupo8'@'localhost';
