7c0a6b89efead3f8902cb0669a23d5b2e8be9441
Mauro Molina <molinamauro2@gmail.com>
2020-05-07 22:17:45 -0300

    Agregue las anotaciones Transactional

--- src/main/java/ar/edu/unlp/info/bd2/services/DBliveryServiceImpl.java
+++ src/main/java/ar/edu/unlp/info/bd2/services/DBliveryServiceImpl.java
@@ -16,7 +16,7 @@ import ar.edu.unlp.info.bd2.model.User;
 import ar.edu.unlp.info.bd2.repositories.DBliveryException;
 import ar.edu.unlp.info.bd2.repositories.DBliveryRepository;
 
-@Transactional
+
 public class DBliveryServiceImpl implements DBliveryService {
 
 	private DBliveryRepository repository;
@@ -29,6 +29,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order addProduct(Long order, Long quantity, Product product) throws DBliveryException {
 		Product prod = this.getProductById(product.getId())
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.PRODUCT_NOT_FOUND));
@@ -42,6 +43,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public boolean canCancel(Long order) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -49,6 +51,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order cancelOrder(Long order) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -64,6 +67,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order cancelOrder(Long order, Date date) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -79,6 +83,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public boolean canDeliver(Long order) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -86,6 +91,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public boolean canFinish(Long id) throws DBliveryException {
 		Order actualOrder = this.getOrderById(id)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -93,6 +99,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order createOrder(Date dateOfOrder, String address, Float coordX, Float coordY, User client) {
 		Order order = new Order(dateOfOrder, address, coordX, coordY, client);
 		repository.saveObject(order);
@@ -100,6 +107,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier) {
 		Product prod = new Product(name, price, weight, supplier);
 		repository.saveObject(prod);
@@ -107,6 +115,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier, Date date) {
 		Product prod = new Product(name, price, weight, supplier, date);
 		repository.saveObject(prod);
@@ -114,6 +123,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Supplier createSupplier(String name, String cuil, String address, Float coordX, Float coordY) {
 		Supplier supplier = new Supplier(name, cuil, address, coordX, coordY);
 		repository.saveObject(supplier);
@@ -121,6 +131,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public User createUser(String email, String password, String username, String name, Date dateOfBirth) {
 		User user = new User(email, password, username, name, dateOfBirth);
 		repository.saveObject(user);
@@ -128,6 +139,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order deliverOrder(Long order, User deliveryUser) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -143,6 +155,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order deliverOrder(Long order, User deliveryUser, Date date) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -158,6 +171,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order finishOrder(Long order) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -172,6 +186,7 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public Order finishOrder(Long order, Date date) throws DBliveryException {
 		Order actualOrder = this.getOrderById(order)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.ORDER_NOT_FOUND));
@@ -186,163 +201,194 @@ public class DBliveryServiceImpl implements DBliveryService {
 	}
 
 	@Override
+	@Transactional
 	public List<User> get5LessDeliveryUsers() {
 		return repository.get5LessDeliveryUsers();
 	}
 
 	@Override
+	@Transactional
 	public OrderStatus getActualStatus(Long order) {
 		Order actualOrder = this.getOrderById(order).get();
 		return actualOrder.getActualStatus();
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getAllOrdersMadeByUser(String username) {
 		return repository.getAllOrdersMadeByUser(username);
 	}
 
 	@Override
+	@Transactional
 	public Product getBestSellingProduct() {
 		return repository.getBestSellingProduct();
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getCancelledOrdersInPeriod(Date startDate, Date endDate) {
 		return repository.getCancelledOrdersInPeriod(startDate, endDate);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersForUser(String username) {
 		return repository.getDeliveredOrdersForUser(username);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersInPeriod(Date startDate, Date endDate) {
 		return repository.getDeliveredOrdersInPeriod(startDate, endDate);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersSameDay() {
 		return repository.getDeliveredOrdersSameDay();
 	}
 
 	@Override
+	@Transactional
 	public Optional<Order> getOrderById(Long id) {
 		Order order = (Order) repository.findById(Order.class, id);
 		return Optional.of(order);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getOrdersCompleteMorethanOneDay() {
 		return repository.getOrdersCompleteMorethanOneDay();
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getOrderWithMoreQuantityOfProducts(Date day) {
 		return repository.getOrderWithMoreQuantityOfProducts(day);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getPendingOrders() {
 		return repository.getPendingOrders();
 	}
 
 	@Override
+	@Transactional
 	public Optional<Product> getProductById(Long id) {
 		Product prod = (Product) repository.findById(Product.class, id);
 		return Optional.of(prod);
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductByName(String name) {
 		ArrayList<Product> prods = (ArrayList<Product>) repository.findProductBySimilarName(name);
 		return prods;
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductIncreaseMoreThan100() {
 		return repository.getProductIncreaseMoreThan100();
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductsNotSold() {
 		return repository.getProductsNotSold();
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductsOnePrice() {
 		return repository.getProductsOnePrice();
 	}
 
 	@Override
+	@Transactional
 	public List<Object[]> getProductsWithPriceAt(Date day) {
 		return repository.getProductsWithPriceAt(day);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getSentMoreOneHour() {
 		return repository.getSentMoreOneHour();
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getSentOrders() {
 		return repository.getSentOrders();
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getSoldProductsOn(Date day) {
 		return repository.getSoldProductsOn(day);
 	}
 
 	@Override
+	@Transactional
 	public Supplier getSupplierLessExpensiveProduct() {
 		return repository.getSupplierLessExpensiveProduct();
 	}
 
 	@Override
+	@Transactional
 	public List<Supplier> getSuppliersDoNotSellOn(Date day) {
 		return repository.getSuppliersDoNotSellOn(day);
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getTop10MoreExpensiveProducts() {
 		return repository.getTop10MoreExpensiveProducts();
 	}
 
 	@Override
+	@Transactional
 	public List<User> getTop6UsersMoreOrders() {
 		return repository.getTop6UsersMoreOrders();
 	}
 
 	@Override
+	@Transactional
 	public List<Supplier> getTopNSuppliersInSentOrders(int n) {
 		return repository.getTopNSuppliersInSentOrders(n);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserByEmail(String email) {
 		User user = repository.findByEmail(email);
 		return Optional.of(user);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserById(Long id) {
 		User user = (User) repository.findById(User.class, id);
 		return Optional.of(user);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserByUsername(String username) {
 		User user = repository.findByUsername(username);
 		return Optional.of(user);
 	}
 
 	@Override
+	@Transactional
 	public List<User> getUsersSpendingMoreThan(Float amount) {
 		return repository.getUsersSpendingMoreThan(amount);
 	}
 
 	@Override
+	@Transactional
 	public Product updateProductPrice(Long id, Float price, Date startDate) throws DBliveryException {
 		Product prod = this.getProductById(id)
 				.orElseThrow(() -> new DBliveryException(DBliveryServiceImpl.PRODUCT_NOT_FOUND));
