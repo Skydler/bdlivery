4cc59f33b6770d8fee1d33163459c0636f950dab
Leonel Mandarino <leonelmandarino@gmail.com>
2020-05-23 16:05:43 -0300

    Delete all but model folder for mongo structure

--- createDatabase.sh
+++
@@ -1,6 +0,0 @@
-mysql -u root -p -e "DROP DATABASE IF EXISTS bd2_grupo8;
-                  CREATE DATABASE bd2_grupo8;
-                  CREATE USER IF NOT EXISTS 'grupo8'@'localhost' IDENTIFIED BY 'grupo8';
-                  GRANT ALL PRIVILEGES ON bd2_grupo8.* TO 'grupo8'@'localhost';
-                  FLUSH PRIVILEGES;
-                  "
