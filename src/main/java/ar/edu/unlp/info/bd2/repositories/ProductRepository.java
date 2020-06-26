c5b77e2ef2be47d78dfe4f10e06d28c0d2a28d63
Leonel Mandarino <leonelmandarino@gmail.com>
2020-06-26 02:33:05 -0300

    Add Transactional annotation

--- src/main/java/ar/edu/unlp/info/bd2/repositories/ProductRepository.java
+++ src/main/java/ar/edu/unlp/info/bd2/repositories/ProductRepository.java
@@ -1,6 +1,5 @@
 package ar.edu.unlp.info.bd2.repositories;
 
-import java.util.Collection;
 import java.util.Date;
 import java.util.List;
 
@@ -11,10 +10,10 @@ import org.springframework.data.repository.query.Param;
 import ar.edu.unlp.info.bd2.model.Product;
 
 public interface ProductRepository extends CrudRepository<Product, Long> {
-	
+
 	@Query("SELECT p FROM Product p WHERE p.name like %:name%")
-	List<Product> findBySimilarName(@Param("name")String name);
-	
+	List<Product> findBySimilarName(@Param("name") String name);
+
 	@Query("SELECT p FROM Order o JOIN o.items i JOIN i.product p WHERE o.orderDate=:day")
 	List<Product> getSoldProductsOn(@Param("day") Date day);
 }
--- src/main/java/ar/edu/unlp/info/bd2/services/SpringDataDBliveryService.java
+++ src/main/java/ar/edu/unlp/info/bd2/services/SpringDataDBliveryService.java
@@ -5,6 +5,7 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.transaction.annotation.Transactional;
 
 import ar.edu.unlp.info.bd2.model.Item;
 import ar.edu.unlp.info.bd2.model.Order;
@@ -19,6 +20,7 @@ import ar.edu.unlp.info.bd2.repositories.SupplierRepository;
 import ar.edu.unlp.info.bd2.repositories.OrderRepository;
 import ar.edu.unlp.info.bd2.repositories.ItemRepository;
 
+
 public class SpringDataDBliveryService implements DBliveryService, DBliveryStatisticsService {
 
 	@Autowired
@@ -31,52 +33,61 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	private OrderRepository orderRepository;
 	@Autowired
 	private ItemRepository itemRepository;
-	
+
 	@Override
+	@Transactional
 	public Product getMaxWeigth() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getAllOrdersMadeByUser(String username) {
 		return orderRepository.getAllOrdersMadeByUser(username);
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getPendingOrders() {
 		return orderRepository.getOrdersInStatus("Pending");
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getSentOrders() {
-		return orderRepository.getOrdersInStatus("Delivered");
+		return orderRepository.getOrdersInStatus("Sending");
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersInPeriod(Date startDate, Date endDate) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public List<Order> getDeliveredOrdersForUser(String username) {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductsOnePrice() {
 		// TODO Auto-generated method stub
 		return null;
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getSoldProductsOn(Date day) {
 		return productRepository.getSoldProductsOn(day);
 	}
 
 	@Override
+	@Transactional
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier) {
 		Product prod = new Product(name, price, weight, supplier);
 		productRepository.save(prod);
@@ -84,6 +95,7 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Product createProduct(String name, Float price, Float weight, Supplier supplier, Date date) {
 		Product prod = new Product(name, price, weight, supplier, date);
 		productRepository.save(prod);
@@ -91,6 +103,7 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Supplier createSupplier(String name, String cuil, String address, Float coordX, Float coordY) {
 		Supplier sup = new Supplier(name, cuil, address, coordX, coordY);
 		supplierRepository.save(sup);
@@ -98,6 +111,7 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public User createUser(String email, String password, String username, String name, Date dateOfBirth) {
 		User usr = new User(email, password, username, name, dateOfBirth);
 		userRepository.save(usr);
@@ -105,35 +119,41 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Product updateProductPrice(Long id, Float price, Date startDate) throws DBliveryException {
 		Product prod = productRepository.findById(id)
-						.orElseThrow(() -> new DBliveryException("No se encontró el producto"));
+				.orElseThrow(() -> new DBliveryException("No se encontró el producto"));
 		prod.addPrice(price, startDate);
 		productRepository.save(prod);
 		return prod;
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserById(Long id) {
 		return userRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserByEmail(String email) {
 		return userRepository.findByEmail(email);
 	}
 
 	@Override
+	@Transactional
 	public Optional<User> getUserByUsername(String username) {
 		return userRepository.findByUsername(username);
 	}
 
 	@Override
+	@Transactional
 	public Optional<Order> getOrderById(Long id) {
 		return orderRepository.findById(id);
 	}
 
 	@Override
+	@Transactional
 	public Order createOrder(Date dateOfOrder, String address, Float coordX, Float coordY, User client) {
 		Order ord = new Order(dateOfOrder, address, coordX, coordY, client);
 		orderRepository.save(ord);
@@ -141,11 +161,11 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order addProduct(Long order, Long quantity, Product product) throws DBliveryException {
 		Product prod = productRepository.findById(product.getId())
-						.orElseThrow(() -> new DBliveryException("Producto no encontrado"));
-		Order ord = orderRepository.findById(order)
-						.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+				.orElseThrow(() -> new DBliveryException("Producto no encontrado"));
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
 		Item item = new Item(quantity, prod);
 		itemRepository.save(item);
 		ord.addProduct(item);
@@ -154,10 +174,10 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order deliverOrder(Long order, User deliveryUser) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-					.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
-		if(!this.canDeliver(order)) {
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		if (!this.canDeliver(order)) {
 			throw new DBliveryException("La orden no se puede entregar");
 		} else {
 			ord.setDeliveryUser(deliveryUser);
@@ -169,10 +189,10 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order deliverOrder(Long order, User deliveryUser, Date date) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
-		if(!this.canDeliver(order)) {
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		if (!this.canDeliver(order)) {
 			throw new DBliveryException("La orden no se puede entregar");
 		} else {
 			ord.setDeliveryUser(deliveryUser);
@@ -184,10 +204,10 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order cancelOrder(Long order) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
-		if(!this.canCancel(order)) {
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		if (!this.canCancel(order)) {
 			throw new DBliveryException("La orden no se puede cancelar");
 		} else {
 			ord.getStatus().add(new OrderStatus(Order.CANCELLED));
@@ -198,10 +218,10 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order cancelOrder(Long order, Date date) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
-		if(!this.canCancel(order)) {
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		if (!this.canCancel(order)) {
 			throw new DBliveryException("La orden no se puede cancelar");
 		} else {
 			ord.getStatus().add(new OrderStatus(Order.CANCELLED, date));
@@ -212,10 +232,10 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order finishOrder(Long order) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
-		if(!this.canFinish(order)) {
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		if (!this.canFinish(order)) {
 			throw new DBliveryException("La orden no se puede finalizar");
 		} else {
 			ord.getStatus().add(new OrderStatus(Order.DELIVERED));
@@ -226,10 +246,10 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public Order finishOrder(Long order, Date date) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
-		if(!this.canFinish(order)) {
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		if (!this.canFinish(order)) {
 			throw new DBliveryException("La orden no se puede finalizar");
 		} else {
 			ord.getStatus().add(new OrderStatus(Order.CANCELLED, date));
@@ -240,33 +260,35 @@ public class SpringDataDBliveryService implements DBliveryService, DBliveryStati
 	}
 
 	@Override
+	@Transactional
 	public boolean canCancel(Long order) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
 		return ord.getActualStatus().getStatus().equals(Order.PENDING);
 	}
 
 	@Override
+	@Transactional
 	public boolean canFinish(Long id) throws DBliveryException {
-		Order ord = orderRepository.findById(id)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		Order ord = orderRepository.findById(id).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
 		return !ord.isItemsEmpty() && ord.getActualStatus().getStatus().equals(Order.SENDING);
 	}
 
 	@Override
+	@Transactional
 	public boolean canDeliver(Long order) throws DBliveryException {
-		Order ord = orderRepository.findById(order)
-				.orElseThrow(() -> new DBliveryException("Orden no encontrada"));
+		Order ord = orderRepository.findById(order).orElseThrow(() -> new DBliveryException("Orden no encontrada"));
 		return !ord.isItemsEmpty() && ord.getActualStatus().getStatus().equals(Order.PENDING);
 	}
 
 	@Override
+	@Transactional
 	public OrderStatus getActualStatus(Long order) {
 		Order ord = orderRepository.findById(order).get();
 		return ord.getActualStatus();
 	}
 
 	@Override
+	@Transactional
 	public List<Product> getProductsByName(String name) {
 		return productRepository.findBySimilarName(name);
 	}
